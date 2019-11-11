c2dbeccf90441f8828206747e0ae715cb4f172fc
Bruno Marini <bruno.marini1@gmail.com>
2019-11-11 17:01:48 -0200

    Welcomed [+] {table}

--- src/views/acolhido/Acolhido.vue
+++ src/views/acolhido/Acolhido.vue
@@ -14,14 +14,41 @@
 				</router-link>
 			</div>
 		</header>
-		<article>
-
+		<article class="mt-4">
+			<div class="_card p-3">
+				<b-table
+					class="_rounded"
+					:items="items"
+					:fields="fields"
+					:busy="isBusy"
+					primary-key="id"
+					head-variant="dark"
+					fixed responsive bordered hover
+				>
+				<template v-slot:cell(actions)="data">
+					<router-link :to="`/acolhido/editar/${data.item.id}`">
+						<button type="button" name="button" class="edit-btn btn" title="Editar">
+							<edit-icon size="1.5x" class="edit-icon"></edit-icon>
+						</button>
+					</router-link>
+					<button type="button" name="button" class="edit-btn btn" title="Deletar">
+						<trash-2-icon size="1.5x" class="delete-icon"></trash-2-icon>
+					</button>
+	      </template>
+				<template v-slot:table-busy>
+	        <div class="text-center text-dark my-2">
+	          <b-spinner class="align-middle"></b-spinner>
+	          <strong>Carregando...</strong>
+	        </div>
+	      </template>
+				</b-table>
+			</div>
 		</article>
 	</div>
 </template>
 
 <script>
-import { UsersIcon, PlusIcon } from 'vue-feather-icons'
+import { UsersIcon, PlusIcon, EditIcon, Trash2Icon, } from 'vue-feather-icons'
 
 
 import axios from 'axios';
@@ -32,11 +59,35 @@ export default {
 	name: 'acolhido',
 	components: {
 		UsersIcon,
-		PlusIcon
+		PlusIcon,
+		EditIcon,
+		Trash2Icon,
 	},
 	data() {
 		return {
-
+			fields: [
+        {
+          key: 'name',
+					label: 'Nome',
+          sortable: true,
+        },
+        {
+          key: 'age',
+          label: 'Idade',
+          sortable: true,
+        },
+				{
+          key: 'actions',
+          label: 'Ações',
+        },
+      ],
+      items: [
+        { id: 5, isActive: true, age: 40, name: 'Dickerson' },
+        { id: 6, isActive: false, age: 21, name: 'Larsen' },
+        { id: 7, isActive: false, age: 89, name: 'Geneva' },
+        { id: 8, isActive: true, age: 38, name: 'Jami' }
+      ],
+			isBusy: false,
 		}
 	},
 	methods: {
@@ -45,6 +96,11 @@ export default {
 </script>
 
 <style lang="css" scoped>
+/* Busy table styling */
+table.b-table[aria-busy='true'] {
+  opacity: 0.6;
+}
+
 header {
 	color: #707070
 }
