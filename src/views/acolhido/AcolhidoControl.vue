c6c5efe14cd6794d61093d7760ed1a86418f314b
Bruno Marini <bruno.marini1@gmail.com>
2019-11-11 06:58:26 -0200

    Welcomed [^] {edit button}

--- src/views/acolhido/AcolhidoControl.vue
+++ src/views/acolhido/AcolhidoControl.vue
@@ -75,9 +75,13 @@
 				<h5 class="mb-0 px-2"><b>Próxima</b></h5>
 				<chevron-right-icon size="1.5x" class="custom-class"></chevron-right-icon>
 			</button>
-			<button v-else @click="{{addMethod ? adicionar : editar }}" type="button" name="button" class="add-btn btn py-2 px-3 d-flex align-items-center _rounded-100">
+			<button v-else-if="addMethod" @click="adicionar" type="button" name="button" class="add-btn btn py-2 px-3 d-flex align-items-center _rounded-100">
 				<plus-icon size="1.5x" class="add-icon"></plus-icon>
-				<h5 class="mb-0 px-2"><b>{{addMethod ? 'Adicionar' : 'Editar' }}</b></h5>
+				<h5 class="mb-0 px-2"><b>Adicionar</b></h5>
+			</button>
+			<button v-else @click="editar" type="button" name="button" class="edit-btn btn py-2 px-3 pl-4 d-flex align-items-center _rounded-100">
+				<edit-icon size="1.5x" class="edit-icon"></edit-icon>
+				<h5 class="mb-0 px-2"><b>Editar</b></h5>
 			</button>
 		</footer>
 	</div>
@@ -92,7 +96,7 @@ import FormStep3 from '@/views/acolhido/step/AcolhidoStep3.vue';
 import FormStep4 from '@/views/acolhido/step/AcolhidoStep4.vue';
 import FormStep5 from '@/views/acolhido/step/AcolhidoStep5.vue';
 
-import { UsersIcon, PlusIcon, ChevronRightIcon, ArrowLeftIcon, HeartIcon } from 'vue-feather-icons'
+import { UsersIcon, PlusIcon, ChevronRightIcon, ArrowLeftIcon, HeartIcon, EditIcon } from 'vue-feather-icons'
 
 import axios from 'axios';
 // BUGFIX: same Vue CLI Service URL for CORS with Cue CLI proxy (look at "vue.config.js" file)
@@ -107,6 +111,7 @@ export default {
 		ChevronRightIcon,
 		ArrowLeftIcon,
 		HeartIcon,
+		EditIcon,
 		FormStep1,
 		FormStep2,
 		FormStep3,
@@ -154,6 +159,11 @@ header {
 .add-btn, .next-btn {
 	color: white;
 	background-color: #175D2B;
+}
+
+.edit-btn {
+	color: black;
+	background-color: #E3DB4A;
 }
 
 .cancel-btn {
