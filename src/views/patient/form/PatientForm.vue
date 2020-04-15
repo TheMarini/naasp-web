0a73011f785300b0463a1bd434c73cccc7aa60c2
Bruno Marini <bruno.marini1@gmail.com>
2020-04-15 05:14:18 -0300

    [*] ESLint fix

--- src/views/patient/form/PatientForm.vue
+++ src/views/patient/form/PatientForm.vue
@@ -1,128 +1,135 @@
 <template lang="html">
-	<div class="acolhido p-4">
-		<header class="d-flex justify-content-between align-items-center">
-			<div class="d-flex align-items-center">
-				<heart-icon size="2.3x" class="title-icon"></heart-icon>
-				<h2 class="ml-3 mb-0"><b>{{addMethod ? 'Adicionar' : 'Editar' }} acolhido</b></h2>
-			</div>
-			<div class="steps d-flex">
-				<step
-					class="step ml-2"
-					v-for="index of steps"
-					@click.native="currentStep = index"
-					:number="index"
-					:active="index <= currentStep"
-					:addMethod="addMethod"
-				></step>
-			</div>
-		</header>
-		<div class="wrapper pt-4">
-			<div class="infos">
-				<p class="current-step">ETAPA {{currentStep}}</p>
-			</div>
-			<article>
-				<div @click="currentStep--" v-if="currentStep > 1" class="d-inline-flex prev-btn mb-2">
-					<arrow-left-icon size="1.5x" class="custom-class"></arrow-left-icon>
-					<p class="mb-0 ml-1">Voltar a etapa anterior</p>
-				</div>
+  <div class="acolhido p-4">
+    <header class="d-flex justify-content-between align-items-center">
+      <div class="d-flex align-items-center">
+        <heart-icon size="2.3x" class="title-icon"></heart-icon>
+        <h2 class="ml-3 mb-0"><b>{{addMethod ? 'Adicionar' : 'Editar' }} acolhido</b></h2>
+      </div>
+      <div class="steps d-flex">
+        <step
+          class="step ml-2"
+          v-for="index of steps"
+          :key="index"
+          :number="index"
+          :active="index <= currentStep"
+          :addMethod="addMethod"
+          @click.native="currentStep = index"
+        ></step>
+      </div>
+    </header>
+    <div class="wrapper pt-4">
+      <div class="infos">
+        <p class="current-step">ETAPA {{currentStep}}</p>
+      </div>
+      <article>
+        <div @click="currentStep--" v-if="currentStep > 1" class="d-inline-flex prev-btn mb-2">
+          <arrow-left-icon size="1.5x" class="custom-class"></arrow-left-icon>
+          <p class="mb-0 ml-1">Voltar a etapa anterior</p>
+        </div>
 
-				<!-- <VueCodeHighlight>
-				 {{JSON.stringify(welcomed, null, 2)}}
-				</VueCodeHighlight> -->
+        <!-- <VueCodeHighlight>
+         {{JSON.stringify(welcomed, null, 2)}}
+        </VueCodeHighlight> -->
 
-				<PersonalDataForm
-					v-show="currentStep === 1"
-					:responsibleForm="true"
-					:name.sync="welcomed.name"
-					:rg.sync="welcomed.rg"
-					:cpf.sync="welcomed.cpf"
-					:sex.sync="welcomed.sex"
-					:matrialStatus.sync="welcomed.matrialStatus"
-					:educationLevel.sync="welcomed.educationLevel"
-					:jobRole.sync="welcomed.jobRole"
-					:birthDate.sync="welcomed.birthDate"
-					:age.sync="welcomed.age"
-					:isUnderAge.sync="welcomed.isUnderAge"
-					:religion.sync="welcomed.religion"
-					:placeOfBirth.sync="welcomed.placeOfBirth"
-					:nationality.sync="welcomed.nationality"
-					:publicPlace.sync="welcomed.address.publicPlace"
-					:addressNumber.sync="welcomed.address.number"
-					:addressComplement.sync="welcomed.address.complement"
-					:neighborhood.sync="welcomed.address.neighborhood"
-					:city.sync="welcomed.address.city"
-					:state.sync="welcomed.address.state"
-					:cep.sync="welcomed.address.cep"
-					:cellPhoneNumber.sync="welcomed.cellPhoneNumber"
-					:homePhoneNumber.sync="welcomed.homePhoneNumber"
-					:businessPhoneNumber.sync="welcomed.businessPhoneNumber"
-					:email.sync="welcomed.email"
-					:responsibleName.sync="welcomed.responsible.name"
-					:responsibleRg.sync="welcomed.responsible.rg"
-					:responsibleCpf.sync="welcomed.responsible.cpf"
-				></PersonalDataForm>
+        <PersonalDataForm
+          v-show="currentStep === 1"
+          :responsibleForm="true"
+          :name.sync="welcomed.name"
+          :rg.sync="welcomed.rg"
+          :cpf.sync="welcomed.cpf"
+          :sex.sync="welcomed.sex"
+          :matrialStatus.sync="welcomed.matrialStatus"
+          :educationLevel.sync="welcomed.educationLevel"
+          :jobRole.sync="welcomed.jobRole"
+          :birthDate.sync="welcomed.birthDate"
+          :age.sync="welcomed.age"
+          :isUnderAge.sync="welcomed.isUnderAge"
+          :religion.sync="welcomed.religion"
+          :placeOfBirth.sync="welcomed.placeOfBirth"
+          :nationality.sync="welcomed.nationality"
+          :publicPlace.sync="welcomed.address.publicPlace"
+          :addressNumber.sync="welcomed.address.number"
+          :addressComplement.sync="welcomed.address.complement"
+          :neighborhood.sync="welcomed.address.neighborhood"
+          :city.sync="welcomed.address.city"
+          :state.sync="welcomed.address.state"
+          :cep.sync="welcomed.address.cep"
+          :cellPhoneNumber.sync="welcomed.cellPhoneNumber"
+          :homePhoneNumber.sync="welcomed.homePhoneNumber"
+          :businessPhoneNumber.sync="welcomed.businessPhoneNumber"
+          :email.sync="welcomed.email"
+          :responsibleName.sync="welcomed.responsible.name"
+          :responsibleRg.sync="welcomed.responsible.rg"
+          :responsibleCpf.sync="welcomed.responsible.cpf"
+        ></PersonalDataForm>
 
-				<form-step-2
-					v-show="currentStep === 2"
-					:governmentBenefitName.sync="welcomed.money.governmentBenefit.name"
-					:governmentBenefitValue.sync="welcomed.money.governmentBenefit.value"
-					:familyIncomeComments.sync="welcomed.money.familyIncomeComments"
-					:housingCondition.sync="welcomed.home.housingCondition"
-				></form-step-2>
+        <form-step-2
+          v-show="currentStep === 2"
+          :governmentBenefitName.sync="welcomed.money.governmentBenefit.name"
+          :governmentBenefitValue.sync="welcomed.money.governmentBenefit.value"
+          :familyIncomeComments.sync="welcomed.money.familyIncomeComments"
+          :housingCondition.sync="welcomed.home.housingCondition"
+        ></form-step-2>
 
-				<form-step-3
-					v-show="currentStep === 3"
-					:physicalActivity.sync="welcomed.health.physicalActivity"
-					:medicines.sync="welcomed.health.medicines"
-					:qtdCigarettes.sync="welcomed.health.qtdCigarettes"
-					:qtdDrinks.sync="welcomed.health.qtdDrinks"
-					:familyAlcoholAbuse.sync="welcomed.health.family.alcoholAbuse"
-					:familyDiseases.sync="welcomed.health.family.diseases"
-					:familyMedicines.sync="welcomed.health.family.medicines"
-				></form-step-3>
+        <form-step-3
+          v-show="currentStep === 3"
+          :physicalActivity.sync="welcomed.health.physicalActivity"
+          :medicines.sync="welcomed.health.medicines"
+          :qtdCigarettes.sync="welcomed.health.qtdCigarettes"
+          :qtdDrinks.sync="welcomed.health.qtdDrinks"
+          :familyAlcoholAbuse.sync="welcomed.health.family.alcoholAbuse"
+          :familyDiseases.sync="welcomed.health.family.diseases"
+          :familyMedicines.sync="welcomed.health.family.medicines"
+        ></form-step-3>
 
-				<form-step-4
-					v-show="currentStep === 4"
-					:parish.sync="welcomed.affiliation.parish"
-					:religiousActivities.sync="welcomed.affiliation.religiousActivities"
-				></form-step-4>
+        <form-step-4
+          v-show="currentStep === 4"
+          :parish.sync="welcomed.affiliation.parish"
+          :religiousActivities.sync="welcomed.affiliation.religiousActivities"
+        ></form-step-4>
 
-				<form-step-5
-					v-show="currentStep === 5"
-					:priority.sync="welcomed.priority"
-					:demands.sync="welcomed.others.demands"
-					:comments.sync="welcomed.others.comments"
-				></form-step-5>
-			</article>
-		</div>
-		<footer class="pt-4 pb-2 d-flex justify-content-between align-items-center">
-			<router-link :to="'/acolhido'" @click.native="$destroy()">
-				<button type="button" name="button" class="cancel-btn btn py-2 px-3 d-flex align-items-center _rounded-100">
-					<h5 class="mb-0 px-2"><b>Cancelar</b></h5>
-				</button>
-			</router-link>
-			<button v-if="currentStep < steps" @click="currentStep++" :style="[addMethod ? {} : { color: '#000', backgroundColor: '#E3DB4A' }]" type="button" name="button" class="next-btn btn py-2 px-3 d-flex align-items-center _rounded-100">
-				<h5 class="mb-0 px-2"><b>Próxima</b></h5>
-				<chevron-right-icon size="1.5x" class="custom-class"></chevron-right-icon>
-			</button>
-			<button v-else-if="addMethod" @click="adicionar" type="button" name="button" class="add-btn btn py-2 px-3 d-flex align-items-center _rounded-100">
-				<plus-icon size="1.5x" class="add-icon"></plus-icon>
-				<h5 class="mb-0 px-2"><b>Adicionar</b></h5>
-			</button>
-			<button v-else @click="editar" type="button" name="button" class="edit-btn btn py-2 px-3 pl-4 d-flex align-items-center _rounded-100">
-				<edit-icon size="1.5x" class="edit-icon"></edit-icon>
-				<h5 class="mb-0 px-2"><b>Editar</b></h5>
-			</button>
-		</footer>
-	</div>
+        <form-step-5
+          v-show="currentStep === 5"
+          :priority.sync="welcomed.priority"
+          :demands.sync="welcomed.others.demands"
+          :comments.sync="welcomed.others.comments"
+        ></form-step-5>
+      </article>
+    </div>
+    <footer class="pt-4 pb-2 d-flex justify-content-between align-items-center">
+      <router-link :to="'/acolhido'" @click.native="$destroy()">
+        <button type="button" name="button" class="cancel-btn btn py-2
+          px-3 d-flex align-items-center _rounded-100"
+        >
+          <h5 class="mb-0 px-2"><b>Cancelar</b></h5>
+        </button>
+      </router-link>
+      <button v-if="currentStep < steps" @click="currentStep++"
+        :style="[addMethod ? {} : { color: '#000', backgroundColor: '#E3DB4A' }]"
+        type="button" name="button" class="next-btn btn py-2 px-3 d-flex
+        align-items-center _rounded-100"
+      >
+        <h5 class="mb-0 px-2"><b>Próxima</b></h5>
+        <chevron-right-icon size="1.5x" class="custom-class"></chevron-right-icon>
+      </button>
+      <button v-else-if="addMethod" @click="adicionar" type="button" name="button"
+        class="add-btn btn py-2 px-3 d-flex align-items-center _rounded-100"
+      >
+        <plus-icon size="1.5x" class="add-icon"></plus-icon>
+        <h5 class="mb-0 px-2"><b>Adicionar</b></h5>
+      </button>
+      <button v-else @click="editar" type="button" name="button"
+        class="edit-btn btn py-2 px-3 pl-4 d-flex align-items-center _rounded-100">
+        <edit-icon size="1.5x" class="edit-icon"></edit-icon>
+        <h5 class="mb-0 px-2"><b>Editar</b></h5>
+      </button>
+    </footer>
+  </div>
 </template>
 
 <script>
 // FakeDB
 import fakedb from '@/fakedb/welcomed.json';
-
-// Code highlight
-import { component as VueCodeHighlight } from 'vue-code-highlight';
 
 // Step dot
 import Step from '@/components/Step.vue';
@@ -136,7 +143,7 @@ import FormStep5 from '@/views/patient/form/steps/PatientStep5.vue';
 
 // Icons
 import {
-  UsersIcon, PlusIcon, ChevronRightIcon, ArrowLeftIcon, HeartIcon, EditIcon,
+  PlusIcon, ChevronRightIcon, ArrowLeftIcon, HeartIcon, EditIcon,
 } from 'vue-feather-icons';
 
 import axios from 'axios';
@@ -146,9 +153,7 @@ axios.defaults.baseURL = 'http://localhost:4242';
 export default {
   name: 'acolhidoControl',
   components: {
-    VueCodeHighlight,
     Step,
-    UsersIcon,
     PlusIcon,
     ChevronRightIcon,
     ArrowLeftIcon,
@@ -169,7 +174,7 @@ export default {
   },
   mounted() {
     if (!this.addMethod) {
-      this.getWelcomed(parseInt(this.$route.params.id)).then((welcomed) => {
+      this.getWelcomed(parseInt(this.$route.params.id, 10)).then((welcomed) => {
         this.welcomed = welcomed;
         console.log('Welcomed:', this.welcomed);
       });
@@ -202,7 +207,7 @@ export default {
   methods: {
     async getWelcomed(id) {
       try {
-        await axios.get(`/welcomed/${id}`)
+        return await axios.get(`/welcomed/${id}`)
           .then((response) => response.data);
       } catch (e) {
         console.error(e);
@@ -216,7 +221,7 @@ export default {
         .then((response) => {
           console.log(response);
 
-          if (response.status == 200) {
+          if (response.status === 200) {
             this.$destroy();
             this.$router.push('/acolhido');
           }
@@ -233,42 +238,42 @@ export default {
 @import url('../../../../node_modules/vue-code-highlight/themes/prism-tomorrow.css');
 
 header {
-	color: #707070
+  color: #707070
 }
 
 .title-icon {
-	stroke-width: 3px
+  stroke-width: 3px
 }
 
 .prev-btn {
-	color: #707070;
-	font-weight: 500;
-	cursor: pointer;
+  color: #707070;
+  font-weight: 500;
+  cursor: pointer;
 }
 
 .add-btn, .next-btn {
-	color: white;
-	background-color: #175D2B;
+  color: white;
+  background-color: #175D2B;
 }
 
 .edit-btn {
-	color: black;
-	background-color: #E3DB4A;
+  color: black;
+  background-color: #E3DB4A;
 }
 
 .cancel-btn {
-	color: #707070;
-	border: 4px solid #707070;
+  color: #707070;
+  border: 4px solid #707070;
 }
 
 .step {
-	height: 45px;
-	width: 45px;
-	cursor: pointer;
+  height: 45px;
+  width: 45px;
+  cursor: pointer;
 }
 
 .infos .current-step {
-	color: #707070;
-	font-weight: 600
+  color: #707070;
+  font-weight: 600
 }
 </style>
