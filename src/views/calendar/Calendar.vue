492515c6560fe46a7f52239d978483019b798f23
Bruno Marini <bruno.marini1@gmail.com>
2020-06-03 13:48:06 -0300

    [+] Calendar: month view

--- src/views/calendar/Calendar.vue
+++ src/views/calendar/Calendar.vue
@@ -1,13 +1,52 @@
 <template>
-  <div>
-    Esse é o calendário
+  <div class="calendar p-4">
+    <header class="d-flex justify-content-between align-items-center">
+      <div class="d-flex align-items-center">
+        <calendar-icon size="2.3x" class="title-icon"></calendar-icon>
+        <h2 class="ml-3 mb-0"><b>Agenda</b></h2>
+      </div>
+    </header>
+    <article class="mt-4">
+      <FullCalendar
+        default-view="dayGridMonth"
+        locale="pt-br"
+        :plugins="calendarPlugins"
+      />
+    </article>
   </div>
 </template>
 
 <script>
+// Icons
+import { CalendarIcon } from 'vue-feather-icons';
+
+// FullCalendar
+import FullCalendar from '@fullcalendar/vue';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+
 export default {
   name: 'Calendar',
+  components: {
+    CalendarIcon,
+    FullCalendar,
+  },
+  data() {
+    return {
+      calendarPlugins: [dayGridPlugin, timeGridPlugin],
+    };
+  },
 };
 </script>
 
-<style></style>
+<style>
+/* FullCalendar */
+@import '~@fullcalendar/core/main.css';
+@import '~@fullcalendar/daygrid/main.css';
+@import '~@fullcalendar/timegrid/main.css';
+
+/* TODO: check repeated styles like this */
+header {
+  color: #707070;
+}
+</style>
