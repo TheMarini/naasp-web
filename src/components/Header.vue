4c5517a1598d1f66fcd7a436fc0d52a5e6375515
Bruno Marini <bruno.marini1@gmail.com>
2020-06-03 21:41:46 -0300

    [+] Header component: page title and icon slot

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="d-flex justify-content-between align-items-center">
+    <div class="d-flex align-items-center">
+      <slot name="icon"></slot>
+      <h2 class="ml-3 mb-0">
+        <b>{{ title }}</b>
+      </h2>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped></style>
